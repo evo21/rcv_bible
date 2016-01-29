d14fe66a53b9c4947e40bb029968b4e4cf4a85c9
Evan Koch <evankoch@gmail.com>
2016-01-29 13:07:15 -0500

    initial working version of gemfile

--- lib/rcv_bible.rb
+++ lib/rcv_bible.rb
@@ -5,33 +5,78 @@ class RcvBible
 
   VERSION = "0.0.1"
 
+  SINGLES = { "obadiah" => "Oba 1-21",
+              "obadiah 1" => "Oba 1-21",
+              "obadiah 1:1-21" => "Oba 1-21",
+              "philemon" => "Philem 1-25",
+              "philemon 1" => "Philem 1-25",
+              "philemon 1:1-25" => "Philem 1-25",
+              "2 john" => '2 John 1-13',
+              "2 john 1" => '2 John 1-13',
+              "2 john 1:1-13" => '2 John 1-13',
+              "3 john" => '3 John 1-13',
+              "3 john 1" => '3 John 1-13',
+              "3 john 1:1-13" => '3 John 1-13',
+              "jude" => "Jude 1-24",
+              "jude 1" => "Jude 1-24",
+              "jude 1:1-24" => "Jude 1-24"
+            }
+
 class RcvBible::Extractor
 
   def initialize(input_string)
     @input_string = input_string
-    @parsed_request = parse_input_string(@input_string)
+    @parsed_request = parse_input_string
 
   end
 
-  def self.text_of(input_string)
+  def self.text_of(input_string)  # RcvBible::Extractor('Book Chapter:Verses')
     self.new(input_string).text_of
   end
 
   def text_of
-    response = HTTParty.get("https://api.lsm.org/recver.php?String=#{@parsed_request}&Out=json")
-    return response
+    initial_hash = HTTParty.get("https://api.lsm.org/recver.php?String=#{@parsed_request}").to_h
+    input_string = initial_hash["request"]["inputstring"]
+    initial_verses_array = initial_hash["request"]["verses"]["verse"]
+    message = initial_hash["request"]["message"]
+    if message == "\t"  #keeping hash response instead of JSON response, b/c of API error with their JSON encoding, which gives response unrecognized by rubyJSON.
+      return { input_string => initial_verses_array }
+    elsif message["Bad Reference"]
+      return { input_string => message["Bad Reference"] }
+    else
+      chapter_verse_count = message.sub(/.*?requested /, '').split.first.to_i
+      iterations = chapter_verse_count / 30
+      verses_array = []
+      0.upto(iterations) do |x|
+        if x == iterations
+          additional_verses = HTTParty.get("https://api.lsm.org/recver.php?String=#{input_string}: #{(30 * x) + 1}-#{chapter_verse_count}").to_h["request"]["verses"]["verse"]
+          verses_array << additional_verses
+          return { input_string => verses_array.flatten }
+        else
+          additional_verses = HTTParty.get("https://api.lsm.org/recver.php?String=#{input_string}: #{x * 30 + 1}-#{30 * (x + 1)}").to_h["request"]["verses"]["verse"]
+          verses_array << additional_verses
+        end
+      end
+    end
   end
 
-  def parse_input_string(input_string)
-    @input_string # this is temporary place holder, eventually will need to parse
-                  # input_string to return a string form acceptable to LSM API
+
+  def parse_input_string
+    @input_string.gsub('"', "%27")
+    if SINGLES[@input_string.downcase]
+      SINGLES[@input_string.downcase]
+                    # this is temporary place holder, eventually this
+                    # will need to parse input-string to return a 
+                    # string form acceptable to LSM API for all requests
+    else
+      @input_string
+    end
   end
+
+  private
+    def more_than_thirty_verses?
+      @verses.size > 30
+    end
+
 end
-
-
-
-
-
-
-
 end
--- spec/rcv_bible_spec.rb
+++ spec/rcv_bible_spec.rb
@@ -7,8 +7,34 @@ describe RcvBible do
   it 'has a version number' do
     expect(RcvBible::VERSION).not_to be nil
   end
+end
 
-  it 'returns some data' do
-    expect(RcvBible::Extractor.text_of("Psalm 23")).not_to be nil
+describe RcvBible::Extractor do
+  describe "#text_of" do
+    it 'returns some value--change this to Data Typer later' do
+      expect(RcvBible::Extractor.text_of("Psalm 23").class).to eq Hash
+    end
+    it 'returns full verse list in chapters with more than 30 verses' do
+      expect(RcvBible::Extractor.text_of("Psalm 119")["Psalm 119"].count).to eq 176
+    end
+    it 'returns correct verses when verses are submitted with chapter ref' do
+      expect(RcvBible::Extractor.text_of("Psalm 119:1-30")["Psalm 119:1-30"].count).to eq 30
+#      expect(RcvBible::Extractor.text_of("Psalm 119:1-35")["Psalm 119:1-35"].count).to eq 35
+    end
+    it 'returns same book/chapter hash key as the (valid) argument submission' do
+      expect(RcvBible::Extractor.text_of("Mat 10").keys.first).to eq "Mat 10"
+    end
+    it 'returns verse list in chapters with fewer than 30 verses' do
+      expect(RcvBible::Extractor.text_of("Psalm 23")["Psalm 23"].count).to eq 6
+      expect(RcvBible::Extractor.text_of("1 John 1")["1 John 1"].count).to eq 10
+      expect(RcvBible::Extractor.text_of("Jon 1")["Jon 1"].count).to eq 17
+    end
+    it 'returns verse list from books with only one chapter' do
+      expect(RcvBible::Extractor.text_of("Jude")["Jude 1-24"].count).to eq 24
+      expect(RcvBible::Extractor.text_of("3 john")['3 John 1-13'].count).to eq 13
+    end
+    it 'returns error message when passed bad book/chapter arguments' do
+      expect(RcvBible::Extractor.text_of("Awesome")["Awesome"]).to eq "Bad Reference"
+    end
   end
 end
