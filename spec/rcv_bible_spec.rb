db78eb2620faab61fd79dca42868c5f23c09b9d5
Evan Koch <evankoch@gmail.com>
2016-01-29 17:32:12 -0500

    WIP: refactor to smaller methods

--- spec/rcv_bible_spec.rb
+++ spec/rcv_bible_spec.rb
@@ -9,41 +9,44 @@ describe RcvBible do
   end
 end
 
-describe RcvBible::Extractor do
+describe RcvBible::Reference do
   describe "#text_of" do
+    it "foo" do
+
+    end
     it 'returns some value' do
-      expect(RcvBible::Extractor.text_of("Psalm 23")).not_to be nil
-      expect(RcvBible::Extractor.text_of("Matt 1").class).to eq Hash
+      expect(RcvBible::Reference.text_of("Psalm 23")).not_to be nil
+      expect(RcvBible::Reference.text_of("Matt 1").class).to eq Hash
     end
     it 'returns full verse list in chapters with more than 30 verses' do
-      expect(RcvBible::Extractor.text_of("Psalm 119")["Psalm 119"].count).to eq 176
+      expect(RcvBible::Reference.text_of("Psalm 119")["Psalm 119"].count).to eq 176
     end
     it 'returns correct verses when verses are submitted with chapter ref' do
-      expect(RcvBible::Extractor.text_of("Psalm 119:1-30")["Psalm 119:1-30"].count).to eq 30
-#      expect(RcvBible::Extractor.text_of("Psalm 119:1-35")["Psalm 119:1-35"].count).to eq 35
+      expect(RcvBible::Reference.text_of("Psalm 119:1-30")["Psalm 119:1-30"].count).to eq 30
+#      expect(RcvBible::Reference.text_of("Psalm 119:1-35")["Psalm 119:1-35"].count).to eq 35
     end
     it 'returns same book/chapter hash key as the (valid) argument submission' do
-      expect(RcvBible::Extractor.text_of("Mat 10").keys.first).to eq "Mat 10"
+      expect(RcvBible::Reference.text_of("Mat 10").keys.first).to eq "Mat 10"
     end
     it 'returns verse list in chapters with fewer than 30 verses' do
-      expect(RcvBible::Extractor.text_of("Psalm 23")["Psalm 23"].count).to eq 6
-      expect(RcvBible::Extractor.text_of("1 John 1")["1 John 1"].count).to eq 10
-      expect(RcvBible::Extractor.text_of("Jon 1")["Jon 1"].count).to eq 17
+      expect(RcvBible::Reference.text_of("Psalm 23")["Psalm 23"].count).to eq 6
+      expect(RcvBible::Reference.text_of("1 John 1")["1 John 1"].count).to eq 10
+      expect(RcvBible::Reference.text_of("Jon 1")["Jon 1"].count).to eq 17
     end
     it 'returns verse list from books with only one chapter' do
-      expect(RcvBible::Extractor.text_of("Jude")["Jude 1-24"].count).to eq 24
-      expect(RcvBible::Extractor.text_of("3 john")['3 John 1-13'].count).to eq 13
+      expect(RcvBible::Reference.text_of("Jude")["Jude 1-24"].count).to eq 24
+      expect(RcvBible::Reference.text_of("3 john")['3 John 1-13'].count).to eq 13
     end
     it 'returns error message when passed bad book/chapter arguments' do
-      expect(RcvBible::Extractor.text_of("Awesome")["Awesome"]).to eq "Bad Reference"
+      expect(RcvBible::Reference.text_of("Awesome")["Awesome"]).to eq "Bad Reference"
     end
     it 'returns a valid response regardless of single, double, or [no] quotes in submission' do
-      expect(RcvBible::Extractor.text_of("Matt 1")["Matt 1"].count).to eq 25
-      expect(RcvBible::Extractor.text_of('Matt 1')["Matt 1"].count).to eq 25
-      expect(RcvBible::Extractor.text_of("1 Kings 1")["1 Kings 1"].count).to eq 53
-      expect(RcvBible::Extractor.text_of('1 Kings 1')["1 Kings 1"].count).to eq 53
-      expect(RcvBible::Extractor.text_of('2 John 1')["2 John 1-13"].count).to eq 13
-      expect(RcvBible::Extractor.text_of("2 John 1")["2 John 1-13"].count).to eq 13
+      expect(RcvBible::Reference.text_of("Matt 1")["Matt 1"].count).to eq 25
+      expect(RcvBible::Reference.text_of('Matt 1')["Matt 1"].count).to eq 25
+      expect(RcvBible::Reference.text_of("1 Kings 1")["1 Kings 1"].count).to eq 53
+      expect(RcvBible::Reference.text_of('1 Kings 1')["1 Kings 1"].count).to eq 53
+      expect(RcvBible::Reference.text_of('2 John 1')["2 John 1-13"].count).to eq 13
+      expect(RcvBible::Reference.text_of("2 John 1")["2 John 1-13"].count).to eq 13
     end
   end
 end
